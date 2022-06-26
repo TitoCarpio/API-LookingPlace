86060ccd5a2a91874412535fad55f54e9bda587f
TitoCarpio <00226620@uca.edu.sv>
2022-06-26 14:17:26 -0600

    Actualizando base de datos

--- lookingPlace/configuracion/baseDeDatos.js
+++ lookingPlace/configuracion/baseDeDatos.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/lookingPlace", {
+    mongoose.connect(process.env.MONGO_URI , {
         useNewUrlParser: true, useUnifiedTopology: true, 
     })
     .then(() => console.log('Conectado a MongoDB'))
